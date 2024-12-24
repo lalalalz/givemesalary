08c65af1221e30edae2ddfe0b0c3e10a1b183112
lalalalz <lalalalz@naver.com>
2024-12-24 19:38:35 +0900

    Initial commit

---
+++ src/main/java/com/kwt/givemesalary/GivemesalaryApplication.java
@@ -0,0 +1,13 @@
+package com.kwt.givemesalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivemesalaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivemesalaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwt/givemesalary/GivemesalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwt.givemesalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemesalaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
